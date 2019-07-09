3c60f6f322c7f2a046865f828ded8f3357112c11
mrstephenneal <stephen.neal14@gmail.com>
2019-07-09 13:24:22 -0400

    CUT certbot-nginx:v2

--- v2/aws-credentials.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-aws configure set aws_access_key_id ${aws_access_key_id} && \
-aws configure set aws_secret_access_key ${aws_secret_access_key} && \
-aws configure set aws_region_name ${aws_region_name}
-echo AWS S3: credentialed!
