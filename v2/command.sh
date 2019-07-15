5a37ac584c48150fb7e4295dc74c6a7255261d84
mrstephenneal <stephen.neal14@gmail.com>
2019-07-15 12:51:22 -0400

    CUT TODO's

--- v2/command.sh
+++ v2/command.sh
@@ -18,11 +18,6 @@ rm -Rf /etc/letsencrypt/live/${domain} \
     && rm -Rf /etc/letsencrypt/archive/${domain} \
     && rm -Rf /etc/letsencrypt/renewal/${domain}.conf
 
-# TODO: Pull SSL certs from AWS S3
-
-# TODO: Check certificate expiration date
-
-# TODO: run lets encrypt if certificate is going to expire
 # Request Let's Encrypt certificate for ${domain}
 certbot certonly --webroot \
     -w /webroot/certbot \
@@ -37,5 +32,3 @@ certbot certificates
 
 # Keep alive for 60 seconds
 sleep 60
-
-# TODO: Push SSL certs to AWS S3
