ebd7a1b45505ec817d7f8c84cfc3933d04df4244
mrstephenneal <stephen.neal14@gmail.com>
2019-07-17 11:24:50 -0400

    ADD TODO

--- v4/sites-scripts/certify.sh
+++ v4/sites-scripts/certify.sh
@@ -5,6 +5,7 @@
 #  - $email and $staging_cmd have been set by env vars
 
 # Request Let's Encrypt certificate for ${domain}
+# TODO: replace with get request send to validation.hpadesign.com
 certbot certonly --webroot \
     -w /webroot/certbot \
     ${domain_args} \
