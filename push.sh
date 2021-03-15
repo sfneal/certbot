fedd16a32b0e8ee4a33fb36ee3f2b7eb2eb58e6c
Stephen Neal <stephen.neal14@gmail.com>
2021-03-15 12:51:43 -0400

    ADD 'deploy' section to Travis CI config for pushing Docker images to Docker Hub

--- push.sh
+++ push.sh
@@ -2,14 +2,28 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sh "${DIR}"/build.sh
+# Optional TAG argument (if set, only the specified image will be built)
+TAG=${1:-null}
 
-docker push stephenneal/certbot:v1
-docker push stephenneal/certbot:v2
-docker push stephenneal/certbot:v3
-docker push stephenneal/certbot:v4
-docker push stephenneal/certbot:v5
-docker push stephenneal/certbot:v6
-docker push stephenneal/certbot:v7
-docker push stephenneal/certbot:v8
-docker push stephenneal/certbot:v9
+# Check if the TAG variable is set
+if [ "$TAG" != null ]
+
+  # Only build & push one image
+  then
+    sh "${DIR}"/build.sh "${TAG}"
+    docker push stephenneal/certbot:"${TAG}"
+
+  # Build & push all images
+  else
+    sh "${DIR}"/build.sh
+
+    docker push stephenneal/certbot:v1
+    docker push stephenneal/certbot:v2
+    docker push stephenneal/certbot:v3
+    docker push stephenneal/certbot:v4
+    docker push stephenneal/certbot:v5
+    docker push stephenneal/certbot:v6
+    docker push stephenneal/certbot:v7
+    docker push stephenneal/certbot:v8
+    docker push stephenneal/certbot:v9
+fi
